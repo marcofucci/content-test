dfbc9540f5815c27fc6e20299f9bb50f3b4e02ce
Marco Fucci <marcofucci@gmail.com>
2016-07-21 14:14:20 +0100

    expressjs changes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express'),
     path = require('path'),
     os = require('os'),
-    // errorHandler = require('errorhandler'),
     markdown = require('./routes/markdown');
 
 var app = express();
@@ -11,12 +10,6 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.resolve(__dirname, 'public')));
-
-// development only
-// if ('development' == app.get('env')) {
-//     app.use(errorHandler());
-//     app.set('host', 'http://localhost');
-// }
 
 app.get('*', markdown.handler);
 
